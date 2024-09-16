7c13957f4c7647d49b1d9a6140abea65b6c79dce
amittager <tager694@gmail.com>
2024-09-16 21:14:44 +0300

    verify token add

--- services/tokenService.js
+++ services/tokenService.js
@@ -10,4 +10,24 @@ function generateToken(user) {
     return jwt.sign(payload, secretKey, { expiresIn: '1h' });
 }
 
-export  { generateToken };
+
+// Function to verify the token
+function verifyToken(token) {
+    try {
+        const decoded = jwt.verify(token, secretKey);
+
+        return {
+            valid: true,
+            expired: false,
+            decoded,
+        };
+    } catch (error) {
+        return {
+            valid: false,
+            expired: error.message === 'jwt expired',
+            decoded: null,
+        };
+    }
+}
+
+export  { generateToken, verifyToken };
